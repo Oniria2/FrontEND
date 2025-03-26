2be719dfc6cccbd77f50929170f6b08109dbc3e3
AnthoninHelias <heliasanthonin@yahoo.fr>
2025-03-26 10:59:44 +0100

    mise a jour du texte de l'application

--- src/app/result.tsx
+++ src/app/result.tsx
@@ -1,6 +1,7 @@
 import { router, useLocalSearchParams } from "expo-router";
 import { View, Text, StyleSheet, Button, Image } from "react-native";
 import React from 'react';
+/* eslint react/no-unescaped-entities: "off" */
 
 export default function Result() {
     const params = useLocalSearchParams();
@@ -11,17 +12,19 @@ export default function Result() {
             pathname: '/',
         });
     };
-    
+
     return (
         <View style={styles.container}>
+            <Text style={styles.title}>Voici l'image qui représente {'\n'} votre état actuel {params.name}.  </Text>
             <Image
                 // eslint-disable-next-line @typescript-eslint/no-require-imports
                 source={require('../../assets/trophé.png')}
                 style={styles.image}
             />
-            <Text style={styles.title}>Félicitations {params.name}!</Text>
-            <Text style={styles.resultText}>Votre résultat est de {params.score} points !!!</Text>
-            <Button title="Rejouer le Quiz" onPress={handleReplay} color="#8A4FBF" />
+            <Text style={styles.title}>Merci d'avoir pris le temps de répondre a notre questionnaire  {'\n'} {params.name}. </Text>
+            {/* La ligne suivante pour afficher le score, elle doit être mise en commentaire hors des phases de test */}
+            <Text style={styles.resultText}>Votre résultat est de {params.score} </Text>
+            <Button title=" Retour a l'accueil " onPress={handleReplay} color="#8A4FBF" />
         </View>
     );
 }
